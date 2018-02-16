9761727c3ae5b2cbfde0b028beca09110b613286
Glebvsemugolova <Glebvingeod@gmail.com>
2018-02-17 01:36:38 +0300

    Подготовил адаптивную графику

---
+++ source/js/script.js
@@ -0,0 +1,25 @@
+var  link  = document.querySelector(".special-offer__order");
+var popup = document.querySelector(".modal");
+
+if (link) {
+  link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("to-basket--hidden");
+    popup.classList.add("to-basket--active");
+
+  });
+}
+
+var buyArray = document.querySelectorAll(".catalog__buy");
+var modal = document.querySelector(".to-basket");
+
+for (var i = 0; i <buyArray.length; i++) {
+  buyArray[i].addEventListener('click', function(event) {
+    event.preventDefault();
+    modal.classList.remove("to-basket--hidden");
+    modal.classList.add("to-basket--active");
+  });
+
+}
+
+
